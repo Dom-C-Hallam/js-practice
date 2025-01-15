b0881da895cbf622d43a37203e2463f7a2e982cf
Dom-C-Hallam <dominic.hallam.code@gmail.com>
2025-01-15 19:51:41 +0000

    I have added comments to the Javascript code and made a few changes to the CSS

--- colour.js
+++ colour.js
@@ -1,3 +1,4 @@
+//This creates an array containing 8 colours. The array is called colour.
 const colour = [
     "#e84118",
     "#c23616",
@@ -9,9 +10,14 @@ const colour = [
     "#192a56",
 ]
 
+//This creates a variable named "btnwrapper" and selects a <div> element with the class of "btnwrapper".
 const btnwrapper = document.querySelector(".btnwrapper");
+//This creates a variable named button. It selects the only <button> element on the website, in index.html.
 const button = document.querySelector('button');
 
+//This adds an event to the button element when clicked.
+//The function inside tells the button to select one of the 8 arrays above at random, all of the arrays are used.
+//The bottom line specifies that it is the background colour of the btnwrapper <div>. 
 button.addEventListener('click', function() {
     const random = Math.floor(Math.random() * colour.length)
     btnwrapper.style.backgroundColor = colour[random]
